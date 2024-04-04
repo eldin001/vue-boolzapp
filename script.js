e70c7fb3e065a9c358a87da96e714ae1b5db90fe
eldin001 <eldin.balje@icloud.com>
2024-04-04 18:01:38 +0200

    send message implemented

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ createApp({
     data() {
         return {
             contacts: contacts,
-            activeContactId: 1 
+            activeContactId: 1,
+            messageText:''
         }
     },
     methods: {
+        setActiveContactId (id) {
+            this.activeContactId = id;
+        },
+        sendMessage(){
+            const newMessage = {
+                date: new Date().toLocaleString(),
+                message: this.messageText,
+                status: 'sent'
+            }
+            this.activeContact.messages.push(newMessage)
+        }
 
     },
     computed: {
